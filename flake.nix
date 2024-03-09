9f405ee7cb13035dfb9f96bc1f29a39ad061bac9
BrittonR <b@robitzs.ch>
2024-03-09 13:55:09 -0700

    test updated flake.nix to build cargo

--- flake.nix
+++ flake.nix
@@ -2,65 +2,86 @@
   description = "sopsbox";
 
   inputs = {
-    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.91.tar.gz";
-    crane.url = "https://flakehub.com/f/ipetkov/crane/0.16.2.tar.gz";
-    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane = {
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, flake-schemas, nixpkgs, rust-overlay, crane, flake-utils, ... }@inputs:
-    let
-      overlays = [
-        rust-overlay.overlays.default
-        (final: prev: {
-          rustToolchain = final.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; };
-        })
-      ];
-
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    crane,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         craneLib = crane.lib.${system};
-      });
-    in {
-      schemas = flake-schemas.schemas;
 
-      packages = forEachSupportedSystem ({ pkgs, craneLib, ... }: {
-        default = craneLib.buildPackage {
+        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
+
+        # libPath =  with pkgs; lib.makeLibraryPath [
+        # ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          cmake
+          makeWrapper
+        ];
+
+        buildInputs = with pkgs; [
+        ];
+
+        cargoArtifacts = craneLib.buildDepsOnly ({
           src = craneLib.cleanCargoSource (craneLib.path ./.);
-          buildInputs = with pkgs; [ libiconv ];
+          inherit buildInputs nativeBuildInputs;
+          pname = "sopbxo";
+        });
+      in with pkgs; {
+        packages = rec {
+          sopsbox= craneLib.buildPackage {
+            src = craneLib.path ./.;
 
-          # Add any additional crate settings here
-          # Example: doCheck = true;
-        };
-      });
+            inherit buildInputs nativeBuildInputs cargoArtifacts;
 
-      devShells = forEachSupportedSystem ({ pkgs, ... }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustToolchain
-            cargo-bloat
-            cargo-edit
-            cargo-outdated
-            cargo-udeps
-            cargo-watch
-            rust-analyzer
-            curl
-            git
-            jq
-            wget
-            nixpkgs-fmt
-          ];
-          env = {
-            RUST_BACKTRACE = "1";
-            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            postInstall = ''
+              # for _size in "16x16" "32x32" "48x48" "64x64" "128x128" "256x256"; do
+              #     echo $src
+              #     install -Dm644 "$src/data/icons/$_size/apps/ytdlp-gui.png" "$out/share/icons/hicolor/$_size/apps/ytdlp-gui.png"
+              # done
+              # install -Dm644 "$src/data/applications/ytdlp-gui.desktop" -t "$out/share/applications/"
+
+              patchelf --set-rpath ${libPath} $out/bin/sopsbox
+
+              # wrapProgram $out/bin/ytdlp-gui \
+              #   --prefix PATH : ${lib.makeBinPath [ pkgs.gnome.zenity pkgs.libsForQt5.kdialog]}\
+            '';
+
+            GIT_HASH = self.rev or self.dirtyRev;
           };
-        };
-      });
-    };
-}
 
+          default = sopsbox;
+        };
+
+        devShell = mkShell {
+          inherit buildInputs nativeBuildInputs;
+
+          packages = with pkgs; [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+            cargo-watch
+          ];
+          LD_LIBRARY_PATH = "${libPath}";
+        };
+      }) // {
+        overlay = final: prev: {
+          inherit (self.packages.${final.system}) sopsbox;
+        };
+      };
+}
