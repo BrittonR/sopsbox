e7c094204d5ae4de71ee00c26a5dbb620599836c
BrittonR <b@robitzs.ch>
2024-03-10 19:46:58 -0600

    add isDarwin check for buildInputs

--- flake.nix
+++ flake.nix
@@ -11,22 +11,21 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    crane,
-    rust-overlay,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , crane
+    , rust-overlay
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
       let
         craneLib = crane.lib.${system};
 
         pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
 
-        # libPath =  with pkgs; lib.makeLibraryPath [
-        # ];
 
         nativeBuildInputs = with pkgs; [
           pkg-config
@@ -34,32 +33,24 @@
           makeWrapper
         ];
 
-        buildInputs = with pkgs; [ libiconv ];
-
+        buildInputs = [
+        ] ++ lib.optionals pkgs.stdenv.isDarwin [
+          # Additional darwin specific inputs can be set here
+          pkgs.libiconv
+        ];
         cargoArtifacts = craneLib.buildDepsOnly ({
           src = craneLib.cleanCargoSource (craneLib.path ./.);
           inherit buildInputs nativeBuildInputs;
           pname = "sopbox";
         });
-      in with pkgs; {
+      in
+      with pkgs; {
         packages = rec {
-          sopsbox= craneLib.buildPackage {
+          sopsbox = craneLib.buildPackage {
             src = craneLib.path ./.;
 
             inherit buildInputs nativeBuildInputs cargoArtifacts;
 
-            # postInstall = ''
-              # for _size in "16x16" "32x32" "48x48" "64x64" "128x128" "256x256"; do
-              #     echo $src
-              #     install -Dm644 "$src/data/icons/$_size/apps/ytdlp-gui.png" "$out/share/icons/hicolor/$_size/apps/ytdlp-gui.png"
-              # done
-              # install -Dm644 "$src/data/applications/ytdlp-gui.desktop" -t "$out/share/applications/"
-
-              # patchelf --set-rpath ${libPath} $out/bin/sopsbox
-
-              # wrapProgram $out/bin/ytdlp-gui \
-              #   --prefix PATH : ${lib.makeBinPath [ pkgs.gnome.zenity pkgs.libsForQt5.kdialog]}\
-            # '';
 
             GIT_HASH = self.rev or self.dirtyRev;
           };
@@ -79,8 +70,8 @@
           LD_LIBRARY_PATH = "${libPath}";
         };
       }) // {
-        overlay = final: prev: {
-          inherit (self.packages.${final.system}) sopsbox;
-        };
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) sopsbox;
       };
+    };
 }
